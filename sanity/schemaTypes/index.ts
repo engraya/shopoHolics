759b7fd216039d89946e7ab2ea0db99bf30a7154
Engraya <ahmadyakubuaya@gmail.com>
2024-04-07 12:36:27 +0100

    Sanity Config Commit

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,5 @@
+import product from "./product"
+import category from "./category"
+import previewImage from "./previewImage"
+
+export const schemaTypes = [product, category, previewImage]
---
+++ src/app/lib/sanity.ts
@@ -0,0 +1,21 @@
+import { createClient } from "next-sanity";
+import imageUrlBuilder from '@sanity/image-url'
+
+
+export const client = createClient({
+  // Find your project ID and dataset in `sanity.json` in your studio project
+  projectId: "s28w7w9l",
+  dataset: "production",
+  useCdn: true
+  // useCdn == true gives fast, cheap responses using a globally distributed cache.
+  // Set this to false if your application require the freshest possible
+  // data always (potentially slightly slower and a bit more expensive).
+});
+
+
+
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source: any) {
+  return builder.image(source);
+}
