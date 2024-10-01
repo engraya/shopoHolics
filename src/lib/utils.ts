fbcbb975e1c18a1e3d3861908295740441187a3a
Engraya <ahmadyakubuaya@gmail.com>
2024-10-01 08:23:03 +0100

    Fixed Error with Timeout

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,24 +1,22 @@
-import { type ClassValue, clsx } from "clsx"
+import { type ClassValue, clsx } from "clsx";
 import confetti from 'canvas-confetti';
-import { twMerge } from "tailwind-merge"
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
-
-
-
 
 export const runFireworks = () => {
   var duration = 5 * 1000;
   var animationEnd = Date.now() + duration;
   var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
 
-  function randomInRange(min : number, max : number) {
+  function randomInRange(min: number, max: number) {
     return Math.random() * (max - min) + min;
   }
 
-  var interval = setInterval(function() {
+  // Explicitly typing `interval` as `NodeJS.Timeout`
+  var interval: NodeJS.Timeout = setInterval(function () {
     var timeLeft = animationEnd - Date.now();
 
     if (timeLeft <= 0) {
@@ -27,7 +25,17 @@ export const runFireworks = () => {
 
     var particleCount = 50 * (timeLeft / duration);
     // since particles fall down, start a bit higher than random
-    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
-    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      })
+    );
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      })
+    );
   }, 250);
-}
+};
